b498473a582afd13b8f482f06c10f81bdb44f05a
SAPPHIRE XWD <113887511+SAPPHIRE-XWD@users.noreply.github.com>
2023-05-11 09:14:33 +0545

    Update Garryv4x.py

--- Garryv4x.py
+++ Garryv4x.py
@@ -12,14 +12,14 @@ except:
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile("Garry.cpython-311.so"):
-        os.system('curl -L https://github.com/SAPPHIRE-XWD/GARRYv4.x/executables/blob/main/Garry.cpython-311.so') 
-        import Garry
+        os.system('curl -L https://github.com/SAPPHIRE-XWD/GARRYv4.x/executables/blob/main/Garry.cpythonv-311.so') 
+        import Garry.main()
     else:
-        import Garry
+        import Garry.main()
 
 elif bit == '32bit':
     if not os.path.isfile('Garry.cpython-311.so'):
         os.system('curl -L https://github.com/SAPPHIRE-XWD/GARRYv4.x/executables/blob/main/Garry.cpython-311.so')
-        import Garry
+        import Garry.main()
     else:
-        import Garry
+        import Garry.main()
