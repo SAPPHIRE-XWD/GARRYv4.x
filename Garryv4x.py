2a5e90ab98ff3503e85824078c6b8f108614df64
SAPPHIRE XWD <113887511+SAPPHIRE-XWD@users.noreply.github.com>
2023-05-11 09:26:14 +0545

    Update Garryv4x.py

--- Garryv4x.py
+++ Garryv4x.py
@@ -1,25 +1,10 @@
 import os, sys, platform
-
-os.system('rm -rf Garry.cpython-311.so')
-
 try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf Garry.cpython-311.so')
+	 import requests
 except:
-    pass
-
-
-bit = platform.architecture()[0]
+	os.system('pip install requests') 
+bit = platform.architecture()[0] 
 if bit == '64bit':
-    if not os.path.isfile("Garry.cpython-311.so"):
-        os.system('curl -L https://github.com/SAPPHIRE-XWD/GARRYv4.x/executables/blob/main/Garry.cpythonv-311.so') 
-        import Garry.main()
-    else:
-        import Garry.main()
-
+	import Garry
 elif bit == '32bit':
-    if not os.path.isfile('Garry.cpython-311.so'):
-        os.system('curl -L https://github.com/SAPPHIRE-XWD/GARRYv4.x/executables/blob/main/Garry.cpython-311.so')
-        import Garry.main()
-    else:
-        import Garry.main()
+		print('Sorry you device cant support')
